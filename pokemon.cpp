5db3355657f759e2ecda9ff50d1e9fc284117ee8
Anthony Enriquez <tenriquez1818@gmail.com>
2024-09-10 14:15:09 -0500

    added my code

--- pokemon.cpp
+++ pokemon.cpp
@@ -1,1 +1,45 @@
 
+//
+//  pokemon.cpp
+//  
+//
+//  Created by Anthony Enriquez on 9/8/24.
+//
+
+#include "pokemon.h"
+
+
+
+Pokemon::Pokemon(string name, int ndex,Type type1){   
+        _name = name;
+        _ndex = ndex;
+        types[0]= type1;
+
+}
+Pokemon::Pokemon(string name, int ndex, Type type1, Type type2){  //constructormulti-type
+        _name = name;
+        _ndex = ndex;
+        types[0] = type1;
+        types[1]= type2;
+
+        
+}
+string Pokemon::name(){
+    return _name;                                                       //Returns name 
+}
+int Pokemon::Ndex(){
+    return _ndex;                                                       //Returns Ndex
+}
+Pokemon::Type Pokemon::type1(){
+    return types[0];
+
+}
+bool Pokemon::is_multitype() { 
+    
+}
+Pokemon::Type Pokemon::type2(){ 
+    return types[1];
+}
+float Pokemon::damage_multiplier(Type attack_type){
+
+}
